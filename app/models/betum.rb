8298899200914419da1348a65d78dd246d0ff68b
jeffreybiles <bilesjeffrey@gmail.com>
2012-03-26 01:21:58 -0500

    After all the good things I've said about it, that email should feel very validated.

--- app/models/betum.rb
+++ app/models/betum.rb
@@ -1,2 +1,6 @@
 class Betum < ActiveRecord::Base
+  validates :email,
+            :presence => true,
+            :uniqueness => true,
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
 end
