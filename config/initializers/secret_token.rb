a31f815297b352891a856b3c0ef1ed5d0abe52fe
jeffreybiles <bilesjeffrey@gmail.com>
2012-03-26 00:17:02 -0500

    First commit.  Added Eric's assets, added in a few gems, and hooking it up to version control.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oakvale::Application.config.secret_token = '73d3ad96f108a4248b678cb394f073d51c52b5d14f230970f73bb8edd9c5ba0b8342cd97623782f1fe69869ae0c473dc9db22e480b9221a2efd27ec2324dcf87'
