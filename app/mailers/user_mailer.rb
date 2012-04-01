19886b042af6b98bba5addc76adf5e979ac816a5
jeffreybiles <bilesjeffrey@gmail.com>
2012-03-31 20:14:14 -0500

    Trying another tack.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "welcome@talesofoakvale.com"
 
   def beta_signup(user)
-    recipients  "<#{user.email}>"
-    from        "Tales of Oakvale"
-    subject     "Thanks for signing up!"
-    sent_on     Time.now
-    #@email = user.email
-    #mail(to: user.email, subject: "Thanks for signing up for Tales of Oakvale")
+    @email = user.email
+    mail(to: user.email, subject: "Thanks for signing up for Tales of Oakvale")
   end
 end
